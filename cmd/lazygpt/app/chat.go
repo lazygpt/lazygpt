338dae470f426f72e3e6846af7d3e6de41816930
seanj <coder@seanj.dev>
2023-04-15 16:52:29 +0000

    feat(repl): swap to go-survey

--- cmd/lazygpt/app/chat.go
+++ cmd/lazygpt/app/chat.go
@@ -3,17 +3,84 @@
 package app
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"os"
 	"strings"
 
-	"github.com/c-bata/go-prompt"
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/AlecAivazis/survey/v2/terminal"
 	"github.com/spf13/cobra"
 
 	"github.com/lazygpt/lazygpt/pkg/plugin"
 	"github.com/lazygpt/lazygpt/plugin/api"
 	"github.com/lazygpt/lazygpt/plugin/log"
 )
+
+var (
+	ErrFailedSurvey    = errors.New("failed to ask survey")
+	ErrPromptNotString = errors.New("prompt must be a string")
+	ErrPromptEmpty     = errors.New("prompt can not be empty")
+	ErrMessagesNil     = errors.New("messages can not be nil")
+)
+
+type promptExecutor func(context.Context, string) string
+
+// promptValidator is a basic validator for the survey prompt.
+func promptValidator(in interface{}) error {
+	input, ok := in.(string)
+	if !ok {
+		return ErrPromptNotString
+	}
+
+	input = strings.TrimSpace(input)
+
+	if input == "" {
+		return ErrPromptEmpty
+	}
+
+	if input == "exit" {
+		os.Exit(0)
+	}
+
+	return nil
+}
+
+// promptExecutorFactory creates a prompt executor that will send the user's prompt to the given completion plugin.
+// The executor will also append the user's prompt and the plugin's response to the given messages slice.
+func promptExecutorFactory(completion api.Completion, messages *[]api.Message) (promptExecutor, error) {
+	if messages == nil {
+		return nil, ErrMessagesNil
+	}
+
+	return func(ctx context.Context, in string) string {
+		input := strings.TrimSpace(in)
+
+		*messages = append(*messages, api.Message{
+			Role:    "user",
+			Content: input,
+		})
+
+		response, reason, err := completion.Complete(ctx, *messages)
+		if err != nil || response == nil {
+			log.Error(
+				ctx, "failed to complete", err,
+				"response", response,
+				"reason", reason,
+			)
+
+			return ""
+		}
+
+		*messages = append(*messages, api.Message{
+			Role:    response.Role,
+			Content: response.Content,
+		})
+
+		return response.Content
+	}, nil
+}
 
 func InitChatCmd(app *LazyGPTApp) {
 	chatCmd := &cobra.Command{
@@ -48,45 +115,41 @@ func InitChatCmd(app *LazyGPTApp) {
 			}
 
 			var messages []api.Message
-
-			executor := func(in string) {
-				input := strings.TrimSpace(in)
-
-				if input == "" {
-					return
-				}
-
-				if input == "exit" {
-					os.Exit(0)
-				}
-
-				messages = append(messages, api.Message{
-					Role:    "user",
-					Content: input,
-				})
-
-				response, reason, err := completion.Complete(ctx, messages)
-				if err != nil || response == nil {
-					log.Error(
-						ctx, "failed to complete", err,
-						"response", response,
-						"reason", reason,
-					)
-
-					return
-				}
-
-				messages = append(messages, api.Message{
-					Role:    response.Role,
-					Content: response.Content,
-				})
+			executor, err := promptExecutorFactory(completion, &messages)
+			if err != nil {
+				return fmt.Errorf("failed to create prompt executor: %w", err)
 			}
 
-			prompt.New(
-				executor,
-				func(_ prompt.Document) []prompt.Suggest { return []prompt.Suggest{} },
-				prompt.OptionPrefix("> "),
-			).Run()
+			prompt := &survey.Multiline{
+				Message: "You: ",
+				Default: "",
+				Help:    "Type your message and press Enter to send it. Type 'exit' to quit.",
+			}
+
+			for {
+				var input string
+				err := survey.AskOne(
+					prompt,
+					&input,
+					survey.WithValidator(survey.Required),
+					survey.WithValidator(promptValidator))
+				if err != nil {
+					if errors.Is(err, terminal.InterruptErr) {
+						log.Debug(ctx, "got ctrl+c, exiting")
+
+						break
+					}
+
+					return fmt.Errorf("%w: %w", ErrFailedSurvey, err)
+				}
+
+				promptResponse := executor(ctx, input)
+				if promptResponse == "" {
+					log.Warn(ctx, "empty response from plugin")
+				}
+
+				fmt.Printf("LazyGPT: %s", promptResponse)
+			}
 
 			return nil
 		},
